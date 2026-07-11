9fa450fab76d6456e7ccc0bb312bf515edcd80a6
Anabelle-of-the-Wildhunt <mak.noemi5@gmail.com>
2026-07-12 01:30:53 +0200

    Added event which makes "#images" div highlight selected <span> like <select> element

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-import { imgs } from "data.js"
+import { imgs } from "./data.js"
 const selector = document.getElementById("images")
 
+
+// THIS EVENT MAKES "#images" DIV HIGHLIGHT SPAN LIKE SELECT ELEMENT
+document.addEventListener("click", function(e){
+
+    if (e.target.matches(`span`)){
+        document.querySelectorAll(`span`).forEach(function(span){
+        span.classList.remove("highlighted")
+        })
+        e.target.classList.add("highlighted")
+    } else {
+        document.querySelectorAll(`span`).forEach(function(span){
+            span.classList.remove("highlighted")
+        })
+    }
+
+})
