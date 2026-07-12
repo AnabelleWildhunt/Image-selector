15b87e3d64f9da7755972cb542f27d50009411c0
Anabelle-of-the-Wildhunt <mak.noemi5@gmail.com>
2026-07-13 00:14:30 +0200

    Added the function to render the content of the main page. Removed said content from the html file.

--- script.js
+++ script.js
@@ -10,11 +10,51 @@ document.addEventListener("click", function(e){
         span.classList.remove("highlighted")
         })
         e.target.classList.add("highlighted")
+
     } else if (e.target.matches(`li[aria-label="link"]`)) {
-        document.querySelectorAll(`span`).forEach(function(span){
+        document.querySelectorAll(`span[class="pseudo-option"]`).forEach(function(span){
             span.classList.remove("highlighted")
         })
-        console.log("Link!")
+        console.log("Link!") // complete later
+
+    } else if (e.target.matches(`button[class="get-image btn"]`)){
+        document.querySelectorAll(`span[class="pseudo-option"]`).forEach(function(span){
+            span.classList.remove("highlighted")
+        })
+        console.log("Button!") // complete later
+
     }
 
 })
+
+// THIS FUNCTION RENDERS CONTENT OF MAIN PAGE
+function renderMainPage(){
+    
+    let mainContent = ""
+    let imagesContent = ""
+
+    document.querySelector("main").innerHTML = ``
+
+    mainContent += `
+    <section>
+        <div class="flex" id="images"></div>
+    </section>
+
+    <div class="flex checkbox-container">
+        <label for="is-gif">Is a gif?</label>
+        <input type="checkbox" name="is-gif" id="is-gif">
+    </div>
+
+    <button class="get-image btn">Get image~!</button>`
+
+    document.querySelector("main").innerHTML = mainContent
+
+    for (let image of imgs){
+        imagesContent += `
+        <span class="pseudo-option" data-tag="${image.tag}">${image.tag}</span>`
+    }
+
+    document.getElementById("images").innerHTML = imagesContent
+}
+
+renderMainPage()
